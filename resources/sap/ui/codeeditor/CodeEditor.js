f3ca0520ad53fec4f07c67726a21ca78556ca7e2
OpenUI5 <openui5@sap.com>
2019-08-20 17:59:02 +0200

    1.52.32

--- resources/sap/ui/codeeditor/CodeEditor.js
+++ resources/sap/ui/codeeditor/CodeEditor.js
@@ -32,7 +32,7 @@ sap.ui.define([
 	 * @extends sap.ui.core.Control
 	 *
 	 * @author SAP SE
-	 * @version 1.52.31
+	 * @version 1.52.32
 	 *
 	 * @constructor
 	 * @public
--- resources/sap/ui/codeeditor/library.js
+++ resources/sap/ui/codeeditor/library.js
@@ -33,7 +33,7 @@ sap.ui.define(['sap/ui/core/Core'],
 		],
 		elements: [],
 		noLibraryCSS: false,
-		version: "1.52.31"
+		version: "1.52.32"
 	});
 
 
