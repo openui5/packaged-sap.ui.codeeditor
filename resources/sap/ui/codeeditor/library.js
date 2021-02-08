f6f17df7c7213438460777629df6bfdcf62e367a
OpenUI5 <openui5@sap.com>
2021-02-08 17:50:41 +0100

    1.52.47

--- resources/sap/ui/codeeditor/library.js
+++ resources/sap/ui/codeeditor/library.js
@@ -1,6 +1,6 @@
 /*!
  * UI development toolkit for HTML5 (OpenUI5)
- * (c) Copyright 2009-2020 SAP SE or an SAP affiliate company.
+ * (c) Copyright 2009-2021 SAP SE or an SAP affiliate company.
  * Licensed under the Apache License, Version 2.0 - see LICENSE.txt.
  */
 
@@ -33,7 +33,7 @@ sap.ui.define(['sap/ui/core/Core'],
 		],
 		elements: [],
 		noLibraryCSS: false,
-		version: "1.52.46"
+		version: "1.52.47"
 	});
 
 
